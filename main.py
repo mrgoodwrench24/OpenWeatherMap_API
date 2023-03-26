2a18d5f6c29013f30d19c3f55c5fba7b6258c260
michaeltroywhite24 <michaeltroywhite24@gmail.com>
2023-03-25 22:10:03 -0600

    3.25.23.5

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from openweather_api import *
 zip_code = input("Enter Zip/Postal Code\n")
 city_dict = get_city(zip_code)
 current_conditions_dict = get_current_conditions(city_dict['lat'], city_dict['long'])
-print(f"Current Conditions for {city_dict['city']}")
+print(f"\nCurrent Conditions for {city_dict['city']}\n")
 
 print(current_conditions_dict['conditions'])
-print(f"Current temperature: {round(current_conditions_dict['current_temp'])} F and feels like {round(current_conditions_dict['current_feel'])} F")
-print(f"Today will have a low of {round(current_conditions_dict['low'])} F and a high of {round(current_conditions_dict['high'])}.")
+print(f"Current temperature: {round(current_conditions_dict['current_temp'])}F and feels like {round(current_conditions_dict['current_feel'])}F")
+print(f"Today will have a low of {round(current_conditions_dict['low'])}F and a high of {round(current_conditions_dict['high'])}F.")
 
 
 
